a0e8353da38cf87ef62233764a5456d60df243ba
Ilya Anisimov <ospiemg@gmail.com>
2024-02-06 22:45:47 +0500

    chore: add server package
    Signed-off-by: Ilya Anisimov <ospiemg@gmail.com>

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"os"
 
+	"github.com/ospiem/gophermart/internal/server"
 	"github.com/rs/zerolog"
 )
 
 func main() {
 	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
-	if err := run(logger); err != nil {
+	if err := server.Run(logger); err != nil {
 		logger.Fatal().Err(err)
 	}
 	logger.Info().Msg("Graceful shutdown completed successfully. All connections closed, and resources released.")
